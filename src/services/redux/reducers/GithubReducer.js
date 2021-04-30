841d165fd5506d8ecf764faee19beb6305a8a0d8
Iqbal Revvin <iqbal.revvin@gmail.com>
2021-04-30 21:44:29 +0700

    Update GithubReducer.js

--- src/services/redux/reducers/GithubReducer.js
+++ src/services/redux/reducers/GithubReducer.js
@@ -8,12 +8,6 @@ const GithubReducer = (state=initialState, action) => {
         case 'GITHUB-FETCH-DATA':
             return{
                 ...state,
-                id: action.payload.id,
-                name: action.payload.name,
-                full_name: action.payload.full_name,
-                owner_name: action.payload.owner_name,
-                owner_avatar: action.payload.owner_avatar,
-                url: action.payload.url,
                 data:action.payload.data
             }
         default:
